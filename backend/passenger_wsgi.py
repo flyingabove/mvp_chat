3095ad959662f41fd607f887e24a7858b52d4302
Christian Gao <gaochris@amazon.com>
2025-11-24 19:06:03 -0800

    changed passenger

--- backend/passenger_wsgi.py
+++ backend/passenger_wsgi.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-# Ensure this directory (prod_backend / beta_backend) is on sys.path
+# Ensure this directory is on the Python path
 sys.path.insert(0, os.path.dirname(__file__))
 
 from app.main import app as application
