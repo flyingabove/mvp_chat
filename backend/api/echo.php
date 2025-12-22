8cacadd0fd7501a557eb3adf9390a1e599e96ce0
Christian Gao <gaochris@amazon.com>
2025-12-22 10:16:12 -0800

    fixed tests

--- backend/api/echo.php
+++
@@ -1,4 +0,0 @@
-<?php
-header('Content-Type: application/json; charset=utf-8');
-$raw=file_get_contents('php://input');
-echo json_encode(['method'=>$_SERVER['REQUEST_METHOD'] ?? 'CLI','raw'=>$raw,'json'=>json_decode($raw,true)]);
